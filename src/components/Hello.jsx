88333a94a613268d19c2e389470e05c6342254aa
Xavier Garcia <xaviergn60@gmail.com>
2020-10-04 16:38:15 -0600

    agregamos stylus con webpack

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,7 +1,6 @@
 import React from "react";
+import "../assets/styles/App.styl";
 
-const Hello = () => {
-  <h1>Hola mundo</h1>;
-};
+const Hello = () => <h1>Hola mundo</h1>;
 
 export default Hello;
